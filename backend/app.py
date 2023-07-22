82670b3ffc5c00ed2c8790a8fa930e88baff0783
Sajid Rahman <sajidrahman@Sajids-MBP.fios-router.home>
2023-07-22 15:32:08 -0400

    explore page initial settings

--- backend/app.py
+++ backend/app.py
@@ -2,5 +2,23 @@ from flask import Flask
 
 app = Flask(__name__)
 
-if __name__ == '__main__':
-    app.run()
+# if __name__ == '__main__':
+#     app.run()
+
+import requests
+API_TOKEN = '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'
+API_URL = "https://api-inference.huggingface.co/models/facebook/bart-large-mnli"
+headers = {"Authorization": f"Bearer {API_TOKEN}"}
+
+sen = input("Sentence: ")
+def query(payload):
+	response = requests.post(API_URL, headers=headers, json=payload)
+	return response.json()
+
+output = query({
+    "inputs": sen,
+    "parameters": {"candidate_labels": ["sports", "music", "fitness"]}
+})
+for i in output['scores']:
+    if (i > .7):
+        print(output['sequence'])
